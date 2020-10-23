0a3275497391246f183dc3e2047419ea766f1c57
apoorvareddy612 <61325120+apoorvareddy612@users.noreply.github.com>
2020-10-23 18:37:33 +0530

    Add files via upload

---
+++ DB.java
@@ -0,0 +1,16 @@
+package a1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+	public static Connection getConnection(){
+		Connection con=null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sys","root","Apoorvaananya612");
+		}catch(Exception e){System.out.println(e);}
+		return con;
+	}
+
+}
